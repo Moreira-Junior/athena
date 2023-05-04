a9c9d939c8a1c0943e30cd5ed62f7f289f7038fa
Nicoly-Almeida <nicoly.almeida@picpay.com>
2023-05-03 22:24:39 -0300

    cnfigurando layout

--- src/main/java/br/edu/ifpb/pweb2/kawa/Athena/controllers/HomeController.java
+++ src/main/java/br/edu/ifpb/pweb2/kawa/Athena/controllers/HomeController.java
@@ -1,17 +1,16 @@
 package br.edu.ifpb.pweb2.kawa.Athena.controllers;
 
-import br.edu.ifpb.pweb2.kawa.Athena.models.Institution;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.ui.Model;
 @Controller
-@RequestMapping("/layout")
 public class HomeController {
-    @GetMapping("/home")
-    public ModelAndView getForm(ModelAndView modelAndView){
-        modelAndView.setViewName("layout/home");
-        return modelAndView;
+
+    @RequestMapping("/home")
+    public String showHomePage(Model m) {
+        m.addAttribute("menu", "home");
+        return "home";
     }
+
 }
+
